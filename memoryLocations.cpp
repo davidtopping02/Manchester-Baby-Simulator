f3da7d92abacffc2eb7e892f67f6dc0e910eef18
davidtopping02 <71976336+davidtopping02@users.noreply.github.com>
2021-11-22 15:20:34 +0000

    Add files via upload

---
+++ memoryLocations.cpp
@@ -0,0 +1,49 @@
+/**
+ * @file memoryLocations.cpp
+ * @author David Topping, Christian Zlatanov, Mathew Gallahcher
+ * @brief register class that has an array representing a 32 bit memory storage 
+ * @version 0.1
+ * @date 2021-11-22
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <iostream>
+#include "memoryLocations.h"
+using namespace std;
+
+/**
+ * @brief gets the 32 position loacation array
+ * 
+ * @return true 
+ * @return false 
+ */
+bool Register::getLocation()
+{
+    return this->location;
+}
+
+/**
+ * @brief sets an value 'true(1) or false(0)' for an indivual memory location
+ * 
+ * @param n 
+ * @param pos 
+ */
+void Register::setLocation(int n, bool pos)
+{
+    this->location[n] = pos;
+}
+
+/**
+ * @brief copys a full register into the specified store location
+ * 
+ * @param inputRegister 
+ * @param reg 
+ */
+void Store::setRegister(bool inputRegister[], int reg)
+{
+    for (int i = 0; i < 32; i++)
+    {
+        this->storeRegister[reg].setLocation(i, inputRegister[i]);
+    }
+}
