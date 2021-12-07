09135f7483fa00a4225092cef32aeac7a01525cd
Conor Kelly <2412359@dundee.ac.uk>
2021-12-07 04:55:02 +0000

    Addition of utility file for storing various utility functions and values

---
+++ assemblerFiles/utility.h
@@ -0,0 +1,24 @@
+/**
+ * @file utility.h
+ * @author Conor Kelly, Stacy Achieng Onyango
+ * @brief file with function declarations for utilities
+ * @version 0.1
+ * @date 2021-11-22
+ *
+ * @copyright Copyright (c) 2021
+ */
+
+#ifndef UTILITY_H
+#define UTILITY_H
+
+using namespace std;
+/**
+ * ### INCLUDES ###
+ */
+#include <iostream>
+#include <string>
+#include "utility.h"
+
+string intToBinary(int n);
+
+#endif
