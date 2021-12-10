967fe21e2fd6172907b31fa3c25ea143cc0b36c6
Conor Kelly <2412359@dundee.ac.uk>
2021-12-10 09:19:58 +0000

    Adding assembler to menu options

--- menu.cpp
+++ menu.cpp
@@ -11,6 +11,9 @@
 #include <iostream>
 #include "manchesterBabyFiles/manchesterBaby.h"
 #include "manchesterBabyFiles/memoryLocations.h"
+
+#include "assemblerFiles/assembler.h"
+
 using namespace std;
 
 /**
@@ -22,67 +25,96 @@ void menu()
     // init local variables
     int choice;
 
-    // menu choices
-    cout << "Please select from one of the following options" << endl;
+    // Initialise the assembler variable
+    Assembler assembler;
 
-    cout << "0. Exit" << endl;
-    cout << "1. Run the manchester baby (default)" << endl;
-    cout << "2. Run the manchester (custom store size)" << endl;
-    cout << "3. Run the assembler" << endl;
-    cout << "4." << endl;
-    cout << "5.\n"
-         << endl;
-
-    do
+    while (true)
     {
-        cout << "Option: ";
-        cin >> choice;
-        cout << endl;
 
-        if (choice < 0 || choice > 5)
-        {
-            cout << "ERROR: enter a valid option"
-                 << endl;
-        }
+        // menu choices
+        cout << "Please select from one of the following options" << endl;
 
-    } while (choice < 0 || choice > 5);
-
-    // running the menu options
-    if (choice == 0)
-    {
-        cout << "Thank you for using the Manchester Baby simulator" << endl;
-        cout << "Exting...\n"
+        cout << "0. Exit" << endl;
+        cout << "1. Run the manchester baby (default)" << endl;
+        cout << "2. Run the manchester (custom store size)" << endl;
+        cout << "3. Run the assembler" << endl;
+        cout << "4. Change the assembler input file" << endl;
+        cout << "5. Change the assembler output file \n"
              << endl;
-        exit(EXIT_SUCCESS);
-    }
-    if (choice == 1)
-    {
-        // running default manchester baby
-        Store *theStore = new Store;
-        manchesterBabySimulator(theStore);
-    }
-    else if (choice == 2)
-    {
-        // running manchester baby with custom store size
-        int size;
-
-        cout << "Enter a store size (must be greater than 8)" << endl;
 
         do
         {
-            cout << "Size: ";
-            cin >> size;
+            cout << "Option: ";
+            cin >> choice;
             cout << endl;
 
-            if (size < 8)
+            if (choice < 0 || choice > 5)
             {
                 cout << "ERROR: enter a valid option"
                      << endl;
             }
-        } while (size < 8);
 
-        Store *theStore = new Store(size);
-        manchesterBabySimulator(theStore);
+        } while (choice < 0 || choice > 5);
+
+        // running the menu options
+        if (choice == 0)
+        {
+            cout << "Thank you for using the Manchester Baby simulator" << endl;
+            cout << "Exting...\n"
+                 << endl;
+            exit(EXIT_SUCCESS);
+        }
+        if (choice == 1)
+        {
+            // running default manchester baby
+            Store *theStore = new Store;
+            manchesterBabySimulator(theStore);
+        }
+        else if (choice == 2)
+        {
+            // running manchester baby with custom store size
+            int size;
+
+            cout << "Enter a store size (must be greater than 8)" << endl;
+
+            do
+            {
+                cout << "Size: ";
+                cin >> size;
+                cout << endl;
+
+                if (size < 8)
+                {
+                    cout << "ERROR: enter a valid option"
+                         << endl;
+                }
+            } while (size < 8);
+
+            Store *theStore = new Store(size);
+            manchesterBabySimulator(theStore);
+        }
+        else if (choice == 3)
+        {
+            assembler.start();
+        }
+        else if (choice == 4)
+        {
+            cout << "Enter the filepath to the input file: " << endl;
+            string filePath;
+
+            cin >> filePath;
+
+            assembler.setInputFile(filePath);
+        }
+        else if (choice == 5)
+        {
+            cout << "Enter the filepath to the output file: " << endl;
+            string filePath;
+
+            cin >> filePath;
+
+            assembler.setOutputFile(filePath);
+        }
     }
 }
 
